413950419dfe0018ae5c68de9b93e41017e3188b
Nikoner <ketanoks@gmail.com>
2024-01-18 21:09:28 +0300

    add SearchForm

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import SearchForm from "../SearchForm";
 import NavBar from "../NavBar";
 import './style.css';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
   <header className="header">
       <div className="logo-left-icons">
         <Link to="/" className="logo">
-          <img src="../image/logo.png" alt="logo" />
+          <img src={logo} alt="logo" />
           <h2 className="logo-name">
             FOX Market
           </h2>
